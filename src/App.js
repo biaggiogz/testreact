39b361b78ff3810a3178fe76367e6d420bfc7201
biaggio <biaggiogzdev@gmail.com>
2022-11-03 13:56:51 +0100

    add cube

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './Cube.css'
+import Cube from 'react-3d-cube';
 
 function App() {
   return (
@@ -10,6 +12,36 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
+	<div>
+        <center>
+            <h1>react-3d-cube</h1>
+            <h2>no children</h2>
+            <div
+            style={{
+                width: 300,
+                height: 300
+            }}
+            >
+            <Cube size={300} index="front" />
+            </div>
+            <h2>set children</h2>
+            <div
+            style={{
+                width: 300,
+                height: 300
+            }}
+            >
+            <Cube size={300} index="front">
+                <div>front</div>
+                <div>right</div>
+                <div>back</div>
+                <div>left</div>
+                <div>top</div>
+                <div>bottom</div>
+            </Cube>
+            </div>
+        </center>
+      </div>
   );
 }
 
